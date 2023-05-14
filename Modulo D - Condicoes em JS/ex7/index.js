29412340e2bbcd53e2a42b5f102e9387af26d6f3
Gustavo <106829052+GustavoKehr@users.noreply.github.com>
2023-05-14 15:24:37 -0300

    adicionando atividades e exercicos

---
+++ Modulo D - Condicoes em JS/ex7/index.js
@@ -0,0 +1,19 @@
+/* CONDICAO SIMPLES */
+
+// var vel = 60.5
+
+// console.log(`Voce esta a ${vel}`)
+
+// if (vel > 80) { 
+//     console.log(`Voce estava a ${vel}, vc foi multado!`) 
+// }
+// console.log(`Dirija sempre com cuidado!`)
+
+
+    var pais = 'franca'
+
+    if(pais != 'Brasil') {
+        console.log(`Voce é estrangeiro do pais: ${pais}`)
+    } else {
+        console.log(`BRASILEIRO!!!`)
+    }
---
+++ Modulo D - Condicoes em JS/ex8/index.js
@@ -0,0 +1,11 @@
+/* Condicao COMPOSTA */
+
+var pais = 'Brasil'
+
+console.log(`Nascido no(na) ${pais}`)
+
+if (pais != 'Brasil') {
+    console.log('Estrangeiro. uuuuuhhhh!!')
+} else {
+    console.log('Brasileiroooooooooo!')
+}
