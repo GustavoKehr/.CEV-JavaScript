235a4724a24559155058e8ad0f1fcae707a05393
Gustavo <106829052+GustavoKehr@users.noreply.github.com>
2023-05-20 13:53:14 -0300

    js poo

---
+++ Modulo F - Avancando os estudos em JS/ex19/obj.js
@@ -0,0 +1,10 @@
+let amigo = {nome:"José",
+idade: 67, 
+sexo: "M", 
+peso: 76.6, 
+engordar(p){
+    console.log('Engordou')
+    this.peso += p
+}}
+amigo.engordar(10)
+console.log(`${amigo.nome} pesa ${amigo.peso}Kg`)
